ec6b51dce14f88ffa85f5e4b269d1e5e895c96b6
bkashadhikari <abikash21@tbc.edu.np>
2021-12-09 23:05:39 +0545

    HelloWorld1

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button countButton;
+    private Button toastButton;
+    private TextView textView;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        countButton= findViewById(R.id.btn_count);
+        toastButton= findViewById(R.id.btn_toast);
+        textView= findViewById(R.id.count_text);
+
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"clicked",Toast.LENGTH_LONG).show();
+            }
+
+
+        });
+        countButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                Log.d("MainActivity",""+count);
+                textView.setText(""+count);
+            }
+        });
     }
 }
